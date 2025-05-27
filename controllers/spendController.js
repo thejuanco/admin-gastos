0b2a85830c9df1fdf8bed469d660b8aaf87841b9
Juanco <juanffo874@gmail.com>
2025-05-26 22:38:29 -0600

    Crea la funcion para crear un gasto (incompleta)

--- controllers/spendController.js
+++ controllers/spendController.js
@@ -1,8 +1,27 @@
+import Budget from "../models/Budget.js"
+import Spend from "../models/Spend.js"
+
 export const createSpend = async (req, res) => {
     try {
-        const { description, amount } = req.body
-        console.log(req.params)
+        const { budget_Id, description, amount } = req.body
+        
+        //Validad que el presupuesto existe
+        const budgetPK = await Budget.findByPk(budget_Id)
+        if(!budgetPK){
+            console.log("El presupuesto no existe")
+        }
+
+        //Crear el gasto
+        const newSpend = await Spend.create({
+            description,
+            amount,
+            budget_Id
+        }
+    )
+
+        return res.status(201).json(newSpend)
     } catch (error) {
-        return res.status(500).json({ message: "Error al crear el gasto", error })
+        res.status(500).json({ message: "Error al crear el gasto", error })
+        console.log(error)
     }
 }
